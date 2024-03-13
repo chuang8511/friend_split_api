39550e785cceefe05c9bfd5f3b541f77633f19c9
chuang8511 <a0961192312@gmail.com>
2024-03-13 15:19:56 +0800

    feat: react router with dom

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import FriendPageIndex from './friend_page';
+import Layout from './Layout';
+import NoPage from './NoPage';
+
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <ItemView
-        imageSrc=''
-        userName='ChunHao'
-        amount={1000}
-        isOwed={true}/>
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        {/* <Route path="/" element={<FriendPageIndex/>}> */}
+          <Route path="friends" element={<FriendPageIndex/>}/>
+          <Route/>
+          <Route/>
+          <Route path="*" element={<NoPage/>}/>
+        </Route>  
+      </Routes>
+    </BrowserRouter>
   )
 };
 
-
-import { createRoot } from 'react-dom/client';
-import ItemView from './friend_page';
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App />);
