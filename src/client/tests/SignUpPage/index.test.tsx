d134cfa8ba6e8f7fc7a2f284784e58f776d6d461
chuang8511 <a0961192312@gmail.com>
2024-03-16 17:56:34 +0800

    feat: test code of calling signup API

--- src/client/tests/SignUpPage/index.test.tsx
+++ src/client/tests/SignUpPage/index.test.tsx
@@ -4,28 +4,59 @@ import { render, fireEvent } from "@testing-library/react";
 import SignUpIndex from "../../SignUpPage";
 
 describe("SignUpIndex Component", () => {
-  test("Submitting the form calls handleSubmit with correct data", () => {
-    const handleSubmit = jest.fn();
-    const { getByLabelText, getByText } = render(<SignUpIndex />);
+  test("Succeed to create an account", async () => {
+    const mockFetch = jest.fn().mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve({ success: true })
+    });
 
-    
-    fireEvent.change(getByLabelText("Your name:"), {
-        target: { value: "testuser" }
-    });
-    fireEvent.change(getByLabelText("Email:"), {
-        target: { value: "test@example.com" }
-    });
-    fireEvent.change(getByLabelText("Password:"), {
-        target: { value: "password123" }
-    });
-    fireEvent.click(getByText("Submit"));
+    const logSpy = jest.spyOn(global.console, 'log');
 
+    (global as any).fetch = mockFetch
+
+    const { getByText } = render(<SignUpIndex />)
+
+    fireEvent.submit(getByText("Submit"));
+
+    await Promise.resolve();
+
+    expect(mockFetch).toHaveBeenCalledWith("http://localhost:3001/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ userName: "", email: "", password: "" })
+    });  
+    expect(logSpy).toHaveBeenCalledTimes(2);
+    expect(logSpy).toHaveBeenCalledWith("Data saved successfully!")
     
-    // expect(handleSubmit).toHaveBeenCalledWith(
-    //   "testuser",
-    //   "test@example.com",
-    //   "password123",
-    //   expect.any(Object) // event object
-    // );
-  });
+  })
+
+  test("Failed to create an account", async() => {
+    const mockFetch = jest.fn().mockResolvedValueOnce({
+      ok: false,
+      status: 500,
+      statusText: "Internal Server Error"
+    });
+    const logSpy = jest.spyOn(global.console, "error");
+
+    (global as any).fetch = mockFetch
+
+    const { getByText } = render(<SignUpIndex />)
+
+    fireEvent.submit(getByText("Submit"));
+
+    await Promise.resolve();
+
+    expect(mockFetch).toHaveBeenCalledWith("http://localhost:3001/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ userName: "", email: "", password: "" })
+    });  
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith("Error saving data:", "Failed to save data")
+
+  })
 });
